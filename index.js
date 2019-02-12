b76f5925351a164a6e7e1c1a0ea177ad6b3ba4ee
Pamela Frieze <pfrieze@gmail.com>
2019-02-12 15:34:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ class Driver {
     this.startDate = new Date(startDate);
   }
   
-  yearsExperienceFromBeginningOf(year) {
-    let endDate = new Date (year, 1, 1); 
-    let endYear = endDate.getFullYear;
-    let startYear = this.startDate.getFullYear;
-    return endYear - startYear;
+  yearsExperienceFromBeginningOf( year ) {
+    let endDate = new Date( year, 1, 1 ); 
+    let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 );
+    return parseInt( totalYears );
+  }
+  
+  route( beginningLocation, endingLocation ) {
+    
   }
 }
+
